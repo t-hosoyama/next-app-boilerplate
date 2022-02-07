9b969f74f25035d2bb7e89720d4a1d2fbd9060fb
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2022-02-07 16:14:23 +0900

    update(conf): update production build setting

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   webpack(config) {
     config.resolve.alias['~'] = path.join(__dirname, 'src')
     return config
