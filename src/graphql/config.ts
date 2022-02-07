6bb9751069272eed865f079378addb6c868fe77d
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2022-02-07 18:50:32 +0900

    update(feat): apply apollo provider

---
+++ src/graphql/config.ts
@@ -0,0 +1,22 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  // Todo: 環境変数から取得
+  uri: 'http://localhost:8080/v1/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import '~/styles/globals.css'
 
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
+import { client } from '~/graphql/config'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <ApolloProvider client={client}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ApolloProvider>
   )
 }
 
