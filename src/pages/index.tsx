58600fcdc9a4bd8c79220adbda8efa699a99a555
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2022-02-07 18:51:16 +0900

    update(feat): organize index page links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,28 +37,12 @@ const Home: NextPage = () => {
             </a>
           </Link>
 
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+          <Link href="/examples/graphql/">
+            <a className={styles.card}>
+              <h2>GraphQL example &rarr;</h2>
+              <p>This is GraphQL example page link.</p>
+            </a>
+          </Link>
         </div>
       </main>
 
