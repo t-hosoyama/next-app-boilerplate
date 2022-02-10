5b796a2f8354a67395583dfe32131800fdc9edae
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2022-02-10 17:16:24 +0900

    fix(feat): fix fallback error page css

--- src/containers/ErrorContainer.tsx
+++ src/containers/ErrorContainer.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import { ContainerLayout } from '~/components/layouts/ContainerLayout'
+import { GlobalStyle } from '~/components/utils/GlobalStyle'
 
 export const ErrorContainer: React.FC = () => {
   return (
-    <ContainerLayout title="システムエラー" backLinkUrl={null}>
-      システムエラーが発生しました。お手数ですが初めから操作を行ってください。
-    </ContainerLayout>
+    <>
+      <GlobalStyle />
+      <ContainerLayout title="システムエラー" backLinkUrl={null}>
+        システムエラーが発生しました。お手数ですが初めから操作を行ってください。
+      </ContainerLayout>
+    </>
   )
 }
