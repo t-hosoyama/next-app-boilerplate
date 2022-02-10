eb6bd2df6f40b7d82530f8bb6a6868d7225961bd
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2022-02-10 12:12:05 +0900

    update(feat): Defining global styles using emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
-import '~/styles/globals.css'
-
 import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
+import { GlobalStyle } from '~/components/utils/GlobalStyle'
 import { client } from '~/graphql/config'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <RecoilRoot>
+        <GlobalStyle />
         <Component {...pageProps} />
       </RecoilRoot>
     </ApolloProvider>
