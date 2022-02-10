bbac8d76832efd0fdae88f03e13813c2be61e6ba
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2022-02-10 14:51:06 +0900

    add(feat): add 404 not found page

---
+++ src/containers/NotFoundContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ContainerLayout } from '~/components/layouts/ContainerLayout'
+
+export const NotFoundContainer: React.FC = () => {
+  return (
+    <ContainerLayout title="404 Not Found">お探しのページは見つかりませんでした。</ContainerLayout>
+  )
+}
---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+import { NotFoundContainer } from '~/containers/NotFoundContainer'
+
+const NotFound: NextPage = () => {
+  return <NotFoundContainer />
+}
+
+export default NotFound
