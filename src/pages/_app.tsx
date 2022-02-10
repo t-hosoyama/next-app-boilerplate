e7a54250beac5a28d6cd99b911bf8b1c83b99993
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2022-02-10 14:54:08 +0900

    update(feat): apply error boundary for app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,21 @@ import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
+import { ErrorBoundary } from '~/components/utils/ErrorBoundary'
 import { GlobalStyle } from '~/components/utils/GlobalStyle'
+import { ErrorContainer } from '~/containers/ErrorContainer'
 import { client } from '~/graphql/config'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <RecoilRoot>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </ApolloProvider>
+    <ErrorBoundary fallback={<ErrorContainer />}>
+      <ApolloProvider client={client}>
+        <RecoilRoot>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </ApolloProvider>
+    </ErrorBoundary>
   )
 }
 
