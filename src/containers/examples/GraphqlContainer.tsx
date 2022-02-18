197e499c9408d5c7002b6a706b321c2ba0ca6c7a
t-hosoyama <terunobu-hosoyama@p3.techfirm.co.jp>
2022-02-18 13:38:53 +0900

    update(feat): update graphql example code

--- src/containers/examples/GraphqlContainer.tsx
+++ src/containers/examples/GraphqlContainer.tsx
@@ -1,15 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import dayjs from 'dayjs'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 import { ContainerLayout } from '~/components/layouts/ContainerLayout'
-import { useUsersQuery } from '~/graphql/users.generated'
+import { useCreateUserMutation, useUsersQuery } from '~/graphql/users.generated'
+
+type UserForm = {
+  name: string
+  birthday: string
+}
 
 export const GraphQLContainer: React.FC = () => {
-  const { loading, error, data } = useUsersQuery()
+  const { isLoading, error, data, refetch } = useUsersQuery()
+  const mutation = useCreateUserMutation({
+    onSuccess: () => refetch(),
+  })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid },
+  } = useForm<UserForm>({
+    mode: 'onChange',
+  })
 
+  const isInvalid = !isDirty || !isValid
   const title = 'GralQL example with apollo client'
 
-  if (loading) {
+  if (isLoading) {
     return (
       <ContainerLayout title={title}>
         <p>Loading...</p>
@@ -26,6 +45,10 @@ export const GraphQLContainer: React.FC = () => {
     )
   }
 
+  const onSubmit = ({ name, birthday }: UserForm) => {
+    mutation.mutate({ name, birthday: new Date(birthday) })
+  }
+
   return (
     <ContainerLayout title={title}>
       {data?.users.map(({ id, name, birthday }) => (
@@ -35,6 +58,57 @@ export const GraphQLContainer: React.FC = () => {
           </li>
         </ul>
       ))}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        css={css`
+          display: flex;
+          justify-content: flex-start;
+          align-items: top;
+          margin-top: 2rem;
+          gap: 1rem;
+        `}
+      >
+        <div>
+          <div>
+            名前:
+            <input
+              type="text"
+              {...register('name', {
+                required: '必須項目です',
+              })}
+            />
+          </div>
+          <span
+            css={css`
+              color: red;
+            `}
+          >
+            {errors.name && errors.name.message}
+          </span>
+        </div>
+        <div>
+          <div>
+            誕生日:
+            <input
+              type="datetime-local"
+              {...register('birthday', {
+                required: '必須項目です',
+              })}
+            />
+          </div>
+          <span
+            css={css`
+              color: red;
+            `}
+          >
+            {errors.birthday && errors.birthday.message}
+          </span>
+        </div>
+        <div>
+          <button disabled={isInvalid}>作成</button>
+        </div>
+      </form>
+      <div>{mutation.isLoading && 'Loading...'}</div>
     </ContainerLayout>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,30 @@
-import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { RecoilRoot } from 'recoil'
 
 import { ErrorBoundary } from '~/components/utils/ErrorBoundary'
 import { GlobalStyle } from '~/components/utils/GlobalStyle'
 import { ErrorContainer } from '~/containers/ErrorContainer'
-import { client } from '~/graphql/config'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // retry: false,
+      // refetchOnWindowFocus: false,
+      staleTime: 1000 * 60,
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary fallback={<ErrorContainer />}>
-      <ApolloProvider client={client}>
+      <QueryClientProvider client={queryClient}>
         <RecoilRoot>
           <GlobalStyle />
           <Component {...pageProps} />
         </RecoilRoot>
-      </ApolloProvider>
+      </QueryClientProvider>
     </ErrorBoundary>
   )
 }
